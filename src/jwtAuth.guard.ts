2212126212be30fc7fc4c04cf5bdb9aa590baa2b
Artem Fedorov <artem@feezback.com>
2024-09-21 20:07:32 +0200

    Small adjustments

--- src/jwtAuth.guard.ts
+++ src/jwtAuth.guard.ts
@@ -1,18 +1,10 @@
-import {
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Response } from 'express';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
-    const response = context.switchToHttp().getResponse<Response>();
+  handleRequest(err: any, user: any) {
     if (err || !user) {
-      // Redirect to login page if user is not authenticated
-      response.redirect('/auth/login'); // replace with your login route
       throw new UnauthorizedException();
     }
 
